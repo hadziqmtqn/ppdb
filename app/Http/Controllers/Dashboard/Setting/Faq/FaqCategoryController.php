a8a6af70a53df4a88ed3057af60eddf21c68b34d
Hadziq <36812793+hadziqmtqn@users.noreply.github.com>
2025-02-13 15:36:18 +0700

    + faq category

--- app/Http/Controllers/Dashboard/Setting/Faq/FaqCategoryController.php
+++ app/Http/Controllers/Dashboard/Setting/Faq/FaqCategoryController.php
@@ -5,35 +5,106 @@ namespace App\Http\Controllers\Dashboard\Setting\Faq;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Faq\FaqCategoryRequest;
 use App\Models\FaqCategory;
+use App\Traits\ApiResponse;
+use Exception;
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controllers\HasMiddleware;
+use Illuminate\Routing\Controllers\Middleware;
+use Illuminate\Support\Facades\Log;
+use Illuminate\View\View;
+use Spatie\Permission\Middleware\PermissionMiddleware;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Yajra\DataTables\Facades\DataTables;
 
-class FaqCategoryController extends Controller
+class FaqCategoryController extends Controller implements HasMiddleware
 {
-    public function index()
+    use ApiResponse;
+
+    public static function middleware(): array
     {
-        return FaqCategory::all();
+        // TODO: Implement middleware() method.
+        return [
+            new Middleware(PermissionMiddleware::using('faq-category-read'), only: ['index', 'datatable']),
+            new Middleware(PermissionMiddleware::using('faq-category-write'), only: ['store', 'update']),
+            new Middleware(PermissionMiddleware::using('faq-category-delete'), only: ['destroy'])
+        ];
     }
 
-    public function store(FaqCategoryRequest $request)
+    public function index(): View
     {
-        return FaqCategory::create($request->validated());
+        $title = 'FAQ';
+
+        return \view('dashboard.settings.faq.faq-category.index', compact('title'));
     }
 
-    public function show(FaqCategory $faqCategory)
+    public function datatable(Request $request): \Illuminate\Http\JsonResponse
     {
-        return $faqCategory;
+        try {
+            if ($request->ajax()) {
+                $data = FaqCategory::query();
+
+                return DataTables::eloquent($data)
+                    ->addIndexColumn()
+                    ->filter(function ($instance) use ($request) {
+                        $search = $request->get('search');
+
+                        $instance->when($search, function ($query) use ($search) {
+                            $query->whereAny(['name'], 'LIKE', '%' . $search . '%');
+                        });
+                    })
+                    ->addColumn('action', function ($row) {
+                        $btn = '<button href="javascript:void(0)" data-slug="'. $row->slug .'" data-name="'. $row->name .'" class="btn btn-icon btn-sm btn-warning" data-bs-toggle="modal" data-bs-target="#modalEdit"><i class="mdi mdi-pencil"></i></button> ';
+                        $btn .= '<button href="javascript:void(0)" data-slug="'. $row->slug .'" class="delete btn btn-icon btn-sm btn-danger"><i class="mdi mdi-delete"></i></button>';
+
+                        return $btn;
+                    })
+                    ->rawColumns(['action'])
+                    ->make();
+            }
+        }catch (Exception $exception) {
+            Log::error($exception->getMessage());
+        }
+
+        return response()->json(true);
     }
 
-    public function update(FaqCategoryRequest $request, FaqCategory $faqCategory)
+    public function store(FaqCategoryRequest $request): JsonResponse
     {
-        $faqCategory->update($request->validated());
+        try {
+            $faqCategory = new FaqCategory();
+            $faqCategory->name = $request->input('name');
+            $faqCategory->save();
+        } catch (Exception $exception) {
+            Log::error($exception->getMessage());
+            return $this->apiResponse('Data gagal disimpan!', null, null, Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
 
-        return $faqCategory;
+        return $this->apiResponse('Data berhasil disimpan!', $faqCategory->name, null, Response::HTTP_OK);
     }
 
-    public function destroy(FaqCategory $faqCategory)
+    public function update(FaqCategoryRequest $request, FaqCategory $faqCategory): JsonResponse
     {
-        $faqCategory->delete();
+        try {
+            $faqCategory->name = $request->input('name');
+            $faqCategory->save();
+        } catch (Exception $exception) {
+            Log::error($exception->getMessage());
+            return $this->apiResponse('Data gagal disimpan!', null, null, Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
 
-        return response()->json();
+        return $this->apiResponse('Data berhasil disimpan!', $faqCategory->name, null, Response::HTTP_OK);
+    }
+
+    public function destroy(FaqCategory $faqCategory): JsonResponse
+    {
+        try {
+            $faqCategory->delete();
+        } catch (Exception $exception) {
+            Log::error($exception->getMessage());
+            return $this->apiResponse('Data gagal dihapus!', null, null, Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+
+        return $this->apiResponse('Data berhasil dihapus!', null, null, Response::HTTP_OK);
     }
 }
--- app/Http/Controllers/Dashboard/Setting/Faq/FaqController.php
+++ app/Http/Controllers/Dashboard/Setting/Faq/FaqController.php
@@ -5,12 +5,39 @@ namespace App\Http\Controllers\Dashboard\Setting\Faq;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Faq\FaqRequest;
 use App\Models\Faq;
+use App\Models\FaqCategory;
+use App\Traits\ApiResponse;
+use Illuminate\Routing\Controllers\HasMiddleware;
+use Illuminate\Routing\Controllers\Middleware;
+use Illuminate\View\View;
+use Spatie\Permission\Middleware\PermissionMiddleware;
 
-class FaqController extends Controller
+class FaqController extends Controller implements HasMiddleware
 {
-    public function index()
+    use ApiResponse;
+
+    public static function middleware(): array
     {
-        return Faq::all();
+        // TODO: Implement middleware() method.
+        return [
+            new Middleware(PermissionMiddleware::using('faq-read'), only: ['index', 'datatable', 'show']),
+            new Middleware(PermissionMiddleware::using('faq-write'), only: ['store', 'update']),
+            new Middleware(PermissionMiddleware::using('faq-delete'), only: ['destroy']),
+        ];
+    }
+
+    public function index(): View
+    {
+        $title = 'FAQ';
+        $faqCategories = $this->getFaqCategories();
+
+        return \view('dashboard.settings.faq.faq.index', compact('title', 'faqCategories'));
+    }
+
+    public function getFaqCategories()
+    {
+        return FaqCategory::select(['id', 'name'])
+            ->get();
     }
 
     public function store(FaqRequest $request)
