2d98cb7c84a2313b6371503b3e665d81bf79edcc
Hadziq <36812793+hadziqmtqn@users.noreply.github.com>
2025-02-22 09:58:27 +0700

    + export excel school report 4 (event)

--- app/Exports/Student/SchoolValueReportExport.php
+++ app/Exports/Student/SchoolValueReportExport.php
@@ -11,17 +11,20 @@ use Illuminate\Support\Collection;
 use Maatwebsite\Excel\Concerns\Exportable;
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\ShouldAutoSize;
+use Maatwebsite\Excel\Concerns\WithEvents;
 use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Events\AfterSheet;
+use PhpOffice\PhpSpreadsheet\Cell\Coordinate;
+use PhpOffice\PhpSpreadsheet\Style\Alignment;
+use PhpOffice\PhpSpreadsheet\Style\Fill;
+use PhpOffice\PhpSpreadsheet\Style\NumberFormat;
 
-class SchoolValueReportExport implements FromCollection, ShouldAutoSize, WithHeadings
+class SchoolValueReportExport implements FromCollection, ShouldAutoSize, WithHeadings, WithEvents
 {
     use Exportable;
 
     protected Request $request;
 
-    /**
-     * @param Request $request
-     */
     public function __construct(Request $request)
     {
         $this->request = $request;
@@ -38,9 +41,6 @@ class SchoolValueReportExport implements FromCollection, ShouldAutoSize, WithHea
         return EducationalGroup::find($this->request->input('educational_group_id'));
     }
 
-    /**
-     * @return Collection
-     */
     public function collection(): Collection
     {
         $lessons = $this->getLessons();
@@ -50,9 +50,8 @@ class SchoolValueReportExport implements FromCollection, ShouldAutoSize, WithHea
             ->whereHas('student', fn($query) => $query->where('educational_institution_id', $this->educationalInstitution()->id))
             ->whereHas('previousSchool', fn($query) => $query->where('educational_group_id', $this->educationalGroup()->id))
             ->whereHas('schoolReports')
-            ->get();
-
-        $users = $users->sortByDesc('school_reports_sum_total_score');
+            ->get()
+            ->sortByDesc('school_reports_sum_total_score');
 
         return $users->map(function (User $user) use ($lessons) {
             static $no = 0;
@@ -61,6 +60,7 @@ class SchoolValueReportExport implements FromCollection, ShouldAutoSize, WithHea
             $row = collect([
                 $no,
                 $user->name,
+                optional($user->student)->registration_number,
                 optional(optional($user->student)->educationalInstitution)->name,
                 optional($user->previousSchool)->school_name
             ]);
@@ -77,8 +77,7 @@ class SchoolValueReportExport implements FromCollection, ShouldAutoSize, WithHea
                 }
             }
 
-            $totalScore = $user->schoolReports->sum('total_score');
-            $row->push($totalScore);
+            $row->push($user->schoolReports->sum('total_score'));
 
             return $row;
         });
@@ -89,11 +88,12 @@ class SchoolValueReportExport implements FromCollection, ShouldAutoSize, WithHea
         $mainHeading = [
             'No',
             'Nama',
+            'No. Registrasi',
             'Lembaga',
             'Asal Sekolah'
         ];
 
-        $semesterHeading = [...array_fill(0, 4, '')]; // baris kosong dibawah mainHeading
+        $semesterHeading = array_fill(0, 5, ''); // baris kosong dibawah mainHeading
 
         $lessons = $this->getLessons();
         foreach ($lessons as $lesson) {
@@ -103,7 +103,7 @@ class SchoolValueReportExport implements FromCollection, ShouldAutoSize, WithHea
                 $mainHeading[] = '';
             }
             foreach ($lesson['semesters'] as $semester) {
-                $semesterHeading[] = $semester;
+                $semesterHeading[] = 'Sem. ' . $semester;
             }
         }
 
@@ -117,24 +117,53 @@ class SchoolValueReportExport implements FromCollection, ShouldAutoSize, WithHea
     {
         $registrationSetting = $this->educationalInstitution()->registrationSetting;
 
-        $semesters = collect();
-        if ($registrationSetting->accepted_with_school_report) {
-            $semesters = collect(json_decode($registrationSetting->school_report_semester, true));
-        }
+        $semesters = $registrationSetting->accepted_with_school_report
+            ? collect(json_decode($registrationSetting->school_report_semester, true))
+            : collect();
 
         return LessonMapping::educationalInstitutionId($this->educationalInstitution()->id)
             ->whereHas('lesson', fn($query) => $query->where('is_active', true))
             ->get()
-            ->filter(function (LessonMapping $lessonMapping) {
-                $previousEducationalGroups = collect(json_decode($lessonMapping->previous_educational_group, true));
-                return $previousEducationalGroups->contains($this->educationalGroup()->id);
-            })
-            ->map(function (LessonMapping $lessonMapping) use ($semesters) {
-                return [
-                    'lessonId' => $lessonMapping->lesson_id,
-                    'lessonCode' => optional($lessonMapping->lesson)->code,
-                    'semesters' => $semesters
-                ];
-            });
+            ->filter(fn($lessonMapping) => collect(json_decode($lessonMapping->previous_educational_group, true))
+                ->contains($this->educationalGroup()->id))
+            ->map(fn($lessonMapping) => [
+                'lessonId' => $lessonMapping->lesson_id,
+                'lessonCode' => optional($lessonMapping->lesson)->code,
+                'semesters' => $semesters
+            ]);
+    }
+
+    public function registerEvents(): array
+    {
+        return [
+            AfterSheet::class => function(AfterSheet $event) {
+                $sheet = $event->sheet->getDelegate();
+                $lastColumnIndex = $sheet->getHighestColumn();
+                $lastColumn = Coordinate::columnIndexFromString($lastColumnIndex);
+                $lastColumnToAdd = Coordinate::stringFromColumnIndex($lastColumn );
+                $totalRows = $sheet->getHighestRow();
+                $sheet->getParent()->getDefaultStyle()->getFont()->setName('Arial');
+                $sheet->getParent()->getDefaultStyle()->getAlignment()
+                    ->setVertical(Alignment::VERTICAL_CENTER);
+
+                $sheet->getStyle('A1:' . $lastColumnToAdd . '2')->getAlignment()
+                    ->setHorizontal(Alignment::HORIZONTAL_CENTER);
+                $sheet->getStyle('A1:' . $lastColumnToAdd . '2')
+                    ->getFont()
+                    ->setBold(true);
+
+                // merge title
+                foreach (['A1:A2','B1:B2','C1:C2','D1:D2','E1:E2'] as $range) {
+                    $sheet->mergeCells($range);
+                }
+
+                // background header
+                $sheet->getStyle('A1:' . $lastColumnIndex . '2')->getFill()->setFillType(Fill::FILL_SOLID)->getStartColor()->setARGB('86D293');
+                // border header
+                $sheet->getStyle('A1:' . $lastColumnIndex . $totalRows)->getBorders()->getAllBorders()->setBorderStyle('thin');
+                // semua baris menggunakan format "text" secara default
+                $sheet->getStyle('A1:' . $lastColumnIndex . $totalRows)->getNumberFormat()->setFormatCode(NumberFormat::FORMAT_TEXT);
+            }
+        ];
     }
 }
