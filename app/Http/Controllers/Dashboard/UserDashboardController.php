a78a3c6caffe5aefdaf02f8b0510c21106290536
Hadziq <36812793+hadziqmtqn@users.noreply.github.com>
2025-02-25 12:48:19 +0700

    feat: enhance user dashboard with registration progress and school report details

--- app/Http/Controllers/Dashboard/UserDashboardController.php
+++ app/Http/Controllers/Dashboard/UserDashboardController.php
@@ -3,14 +3,37 @@
 namespace App\Http\Controllers\Dashboard;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
+use App\Repositories\Student\SchoolReportRepository;
+use App\Repositories\Student\StudentRegistrationRepository;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\View\View;
 
 class UserDashboardController extends Controller
 {
+    protected StudentRegistrationRepository $studentRegistrationRepository;
+    protected SchoolReportRepository $schoolReportRepository;
+
+    /**
+     * UserDashboardController constructor.
+     * @param StudentRegistrationRepository $studentRegistrationRepository
+     * @param SchoolReportRepository $schoolReportRepository
+     */
+
+    public function __construct(StudentRegistrationRepository $studentRegistrationRepository, SchoolReportRepository $schoolReportRepository)
+    {
+        $this->studentRegistrationRepository = $studentRegistrationRepository;
+        $this->schoolReportRepository = $schoolReportRepository;
+    }
+
     public function index(): View
     {
         $title = 'Dashboard';
+        $user = User::with('student.educationalInstitution.registrationSetting')
+            ->findOrFail(Auth::id());
+        $mainProgress = $this->studentRegistrationRepository->menus($user);
+        $schoolReportProgress = $this->schoolReportRepository->isComplete($user);
 
-        return \view('dashboard.user-dashboard.index', compact('title'));
+        return \view('dashboard.user-dashboard.index', compact('title', 'user', 'mainProgress', 'schoolReportProgress'));
     }
 }
---
+++ resources/views/dashboard/user-dashboard/index.blade.php
@@ -0,0 +1,89 @@
+@extends('layouts.master')
+@section('content')
+    <div class="card bg-transparent shadow-none border-0 my-4">
+        <div class="card-body row p-0 pb-3">
+            <div class="col-12 col-md-8 card-separator">
+                <h3 class="display-6">Selamat Datang, <span class="fw-semibold">{{ auth()->user()->name }}</span> 👋🏻</h3>
+                <div class="col-12 col-lg-7">
+                    <p>Berikut ini progres Anda registrasi siswa baru.</p>
+                </div>
+                {{-- <div class="d-flex justify-content-between flex-wrap gap-3 me-5">
+                    <div class="d-flex align-items-center gap-3 me-4 me-sm-0">
+                        <div class="avatar avatar-md">
+                            <div class="avatar-initial bg-label-primary rounded">
+                                <i class="mdi mdi-laptop mdi-36px"></i>
+                            </div>
+                        </div>
+                        <div class="content-right">
+                            <p class="mb-0 fw-medium">Hours Spent</p>
+                            <span class="text-primary mb-0 display-6">34h</span>
+                        </div>
+                    </div>
+                    <div class="d-flex align-items-center gap-3">
+                        <div class="avatar avatar-md">
+                            <div class="avatar-initial bg-label-info rounded">
+                                <i class="mdi mdi-lightbulb-outline mdi-36px"></i>
+                            </div>
+                        </div>
+                        <div class="content-right">
+                            <p class="mb-0 fw-medium">Test Results</p>
+                            <span class="text-info mb-0 display-6">82%</span>
+                        </div>
+                    </div>
+                    <div class="d-flex align-items-center gap-3">
+                        <div class="avatar avatar-md">
+                            <div class="avatar-initial bg-label-warning rounded">
+                                <i class="mdi mdi-check-decagram-outline mdi-36px"></i>
+                            </div>
+                        </div>
+                        <div class="content-right">
+                            <p class="mb-0 fw-medium">Course Completed</p>
+                            <span class="text-warning mb-0 display-6">14</span>
+                        </div>
+                    </div>
+                </div> --}}
+            </div>
+            {{-- <div class="col-12 col-md-4 ps-md-3 ps-lg-5 pt-3 pt-md-0">
+                <div class="d-flex justify-content-between align-items-center">
+                    <div>
+                        <div>
+                            <h5 class="mb-2">Time Spendings</h5>
+                            <p class="mb-4">Weekly report</p>
+                        </div>
+                        <div class="time-spending-chart">
+                            <h3 class="mb-2">231<span class="text-body">h</span> 14<span class="text-body">m</span></h3>
+                            <span class="badge bg-label-success rounded-pill">+18.4%</span>
+                        </div>
+                    </div>
+                    <div id="leadsReportChart"></div>
+                </div>
+            </div> --}}
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-md-6">
+            <div class="card mb-4">
+                <div class="list-group">
+                    <div class="list-group-item active fw-bold">Data Registrasi</div>
+                    @foreach ($mainProgress as $label => $progress)
+                        <a href="{{ $progress['url'] }}" class="list-group-item list-group-item-action d-flex justify-content-between align-items-center">
+                            <div>
+                                <i class="mdi {{ $progress['icon'] }} me-2"></i>{{ $label }}
+                            </div>
+                            <span class="badge bg-{{ $progress['isCompleted'] ? 'primary' : 'warning' }}">{{ $progress['isCompleted'] ? 'Lengkap' : 'Belum Lengkap' }}</span>
+                        </a>
+                    @endforeach
+                    @if (optional(optional(optional($user->student)->educationalInstitution)->registrationSetting)->accepted_with_school_report)
+                        <a href="{{ route('school-report.index', $user->username) }}" class="list-group-item list-group-item-action d-flex justify-content-between align-items-center">
+                            <div>
+                                <i class="mdi mdi-menu me-2"></i>Nilai Rapor
+                            </div>
+                            <span class="badge bg-{{ $schoolReportProgress ? 'primary' : 'warning' }}">{{ $schoolReportProgress ? 'Lengkap' : 'Belum Lengkap' }}</span>
+                        </a>
+                    @endif
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
